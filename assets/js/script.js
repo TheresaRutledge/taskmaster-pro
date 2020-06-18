ff59176aeccf25491a9b2d3699869b0596af65bd
Theresa Rutledge <t.roserutledge@gmail.com>
2020-06-18 13:48:21 -0700

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -173,7 +173,7 @@ $("#task-form-modal").on("shown.bs.modal", function () {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -210,16 +210,18 @@ $('.card .list-group').sortable({
   tolerance: 'pointer',
   helper: 'clone',
   activate: function (event) {
-    // console.log('activate', this);
+    $(this).addClass('dropover');
+    $('.bottom-trash').addClass('bottom-trash-drag');
   },
   deactivate: function (event) {
-    // console.log('deactivate', this);
+    $(this).removeClass('dropover');
+    $('.bottom-trash').removeClass('bottom-trash-drag');
   },
   over: function (event) {
-    // console.log('over', event.target);
+    $(event.target).addClass('dropover-active');
   },
   out: function (event) {
-    // console.log('out', event.target);
+    $(event.target).removeClass('dropover-active');
   },
   update: function (event) {
     var tempArr = [];
@@ -256,10 +258,12 @@ $('.card .list-group').sortable({
       ui.draggable.remove();
     },
     over: function(event, ui){
-      console.log('over');
+      $('.bottom-trash').addClass('bottom-trash-active');
+  
     },
     out: function (event, ui){
-      console.log('out')
+      $('.bottom-trash').removeClass('bottom-trash-active')
+
     }
   });
 
@@ -269,8 +273,8 @@ $('.card .list-group').sortable({
 
   var auditTask = (taskEl) => {
     var date = $(taskEl).find('span').text().trim();
+  
    
-
     var time = moment(date, 'L').set('hour',17);
     
     $(taskEl).removeClass('list-group-item-warning list-group-item-danger');
@@ -281,8 +285,16 @@ $('.card .list-group').sortable({
     else if(Math.abs(moment().diff(time, 'days'))<= 2){
       $(taskEl).addClass('list-group-item-warning');
     }
+   
 
   }
+
+  //audit tasks every 30 min
+  setInterval(function(){
+    $(".card .list-group-item").each(function(){
+      auditTask(this);
+    })
+  },5000)
 
 
 // load tasks for the first time
